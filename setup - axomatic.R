1687cbd557c39d1d6120a92bc18db31f361a43c8
Åsheim <asheimer@ad.helsinki.fi>
2021-12-20 11:11:45 +0100

    Added setup file

---
+++ setup - axomatic.R
@@ -0,0 +1,21 @@
+
+
+setwd("C:/Users/asheimer/files/jottacloud/git")
+#create("axomatic")
+setwd("./axomatic")
+devtools::document()
+setwd("..")
+
+# Rememember to comit with github!
+#.rs.restartR()
+
+#library(Genotools)
+
+  
+#install_github("eiriksen/Genotools")
+#install.packages("devtools")
+
+
+# "C:/Users/asheimer/Jottacloud/git/Genotools/R"
+
+
